e43fe09fa9dc0ac808bd8c8990fd0d98d673a3e5
Kirill Netreba <knete-87@mail.ru>
2023-10-15 15:54:35 +0300

    Update pyproject and add /graphql route

--- graphql_api/__init__.py
+++ graphql_api/__init__.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+
+from graphql_api.schema import schema
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
+    graphql_app = GraphQLRouter(schema)
+    app.include_router(graphql_app, prefix="/graphql")
 
     @app.get("/")
     async def root():
-        return {"message": "Hello World"}
+        return {"message": "For GraphQL go to /graphql"}
 
     return app
